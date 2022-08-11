765571da6711057a9d4bc09ab92f6f266ed89516
sitiLylaPutriKhoirunnisa <110007980+sitiLylaPutriKhoirunnisa@users.noreply.github.com>
2022-08-11 11:02:49 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
- $db = new PDO('mysql:host=localhost;dbname=smkmutu','root','');
+ $db = new PDO("mysql:host=localhost;dbname=smkmutu",'root','');
  $query = $db->query('select * from siswa');
- 
+
  while ($data = $query->fetch() ) :?>
     <p>
         <a href="proseshapus.php?id=<?= $data['id']; ?>">
             <?= $data['nama'] ?>
         </a>
+        
+        <a href="proseshapus.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius:3px">
+            Hapus
+        </a>
     </p>
-    
 <?php endwhile ?>
---
+++ proseshapus.php
@@ -0,0 +1,8 @@
+<?php
+
+$id =$_GET['id'];
+//koneksi
+$db = new PDO("mysql:host=localhost;dbname=smkmutu","root","");
+//query
+$query = $db->query("DELETE FROM siswa where id='$id'");
+
